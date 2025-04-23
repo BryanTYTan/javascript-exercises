a62f5942fb704f50f978cedcb7d94e1eab3c062b
bryan <90520318+BryanTYTan@users.noreply.github.com>
2025-04-23 21:24:37 +0800

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    stringy = ""
 
+    if (parseInt(num) < 0) {
+        stringy = "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            stringy += string;
+        }
+    }
+
+    return stringy
 };
 
 // Do not edit below this line
