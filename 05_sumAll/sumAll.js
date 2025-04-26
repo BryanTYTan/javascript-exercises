d70de7125e67a5b0d3d35072a5d5f655f6fff2c7
bryan <90520318+BryanTYTan@users.noreply.github.com>
2025-04-26 21:42:47 +0800

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof(start) != 'number' || typeof(end) != 'number') {
+        return "ERROR"
+    }
 
+    if (start < 0 || end < 0 || start % 1 !== 0 || end % 1 !== 0) {
+        return "ERROR"        
+    }
+
+    var sum = 0
+    end_num = Math.max(start, end);
+    start_num = Math.min(start, end);
+
+    for (i=start_num; i <= end_num; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
