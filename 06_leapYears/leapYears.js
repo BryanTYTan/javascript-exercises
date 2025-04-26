cd36b2d9234524d8f8d06d346add805311ee72d3
bryan <90520318+BryanTYTan@users.noreply.github.com>
2025-04-26 21:57:05 +0800

    06 Completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    answer = false;
+    if (year % 4 === 0 ) {
+        if (year % 100 === 0 ) {
+            if (year % 400 === 0) {
+                answer = true;
+            }
+        } else {
+            answer = true;
+        }
+    }
 
+
+    return answer
 };
 
 // Do not edit below this line
