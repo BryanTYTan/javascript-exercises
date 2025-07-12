1e7e2def472437e8894948c8c3215b55b95454ed
Bryan Tan <90520318+BryanTYTan@users.noreply.github.com>
2025-07-12 18:49:18 +0800

    Update calculator.js
    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (num_a, num_b) {
+  let final = num_a + num_b;
+  return final;
 };
 
-const subtract = function() {
-	
+const subtract = function (num_a, num_b) {
+  let final = num_a - num_b;
+  return final;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  total = 0
+  for (let i = 0; i < arr.length; i++) {
+    total += Number(arr[i]);
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  total = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    total *= Number(arr[i]);
+  }
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num_a, num_b) {
+  return Math.pow(num_a, num_b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) { return 1; }
+  else { return num * factorial(num - 1); }
 };
 
 // Do not edit below this line
