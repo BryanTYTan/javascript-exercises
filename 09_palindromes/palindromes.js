fc0e1c76e0a71844e83adee6117a813460abbc58
BryanTYTan <90520318+BryanTYTan@users.noreply.github.com>
2025-07-15 21:07:17 +0800

    Updated palindromes using recursion.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let first_letter = text[0];
+    let last_letter = text.slice(-1);
 
+    if (text == "" || text.length == 1) {
+        return true;
+    }
+    else if (first_letter == last_letter) {
+        return palindromes(text.substring(1, text.length-1));
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
