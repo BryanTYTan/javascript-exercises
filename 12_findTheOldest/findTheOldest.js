10815dd06f101786ef759b6b5cd2d19e6f0acb7b
BryanTYTan <90520318+BryanTYTan@users.noreply.github.com>
2025-07-27 17:13:33 +0800

    Completed Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+
+    let sortByAge = array.sort(function (p1, p2) {
+
+        if (!("yearOfDeath" in p1)) {
+            p1_deathyear = new Date().getFullYear();
+        } else {
+            p1_deathyear = p1.yearOfDeath;
+        }
+
+        if (!("yearOfDeath" in p2)) {
+            p2_deathyear = new Date().getFullYear();
+        } else {
+            p2_deathyear = p2.yearOfDeath;
+        }
+
+        let age_1 = p1_deathyear - p1.yearOfBirth;
+        let age_2 = p2_deathyear - p2.yearOfBirth;
+        return age_1 - age_2;
+    });
+
+    return sortByAge[array.length - 1];
 
 };
 
